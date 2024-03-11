c9b82151be50727384b0cbe0fba8c2c79af4f8f6
JakeSCahill <jake@redpanda.com>
2024-03-11 15:50:14 +0000

    Test inline

--- tests/test-cloud-docs.js
+++ tests/test-cloud-docs.js
@@ -1,15 +1,31 @@
 const fs = require('fs');
 const { runTests } = require("doc-detective-core");
 
-const configPath = 'test-configs/cloud/config.json'
+const configPath = 'test-configs/cloud/config.json';
 
 try {
   const rawData = fs.readFileSync(configPath);
   const config = JSON.parse(rawData);
 
   runTests(config)
-    .then(report => console.log('Test Report:', JSON.stringify(report.specs, null, 2)))
-    .catch(error => console.error('Error running tests:', error));
+    .then((report) => {
+      const failedTests = report.specs.flatMap((spec) =>
+        spec.tests.flatMap((test) =>
+          test.contexts.filter(context => context.result === "FAIL")
+        )
+      );
+
+      if (failedTests.length > 0) {
+        console.log('::FAILED_TESTS::' + JSON.stringify(failedTests, null, 2));
+      } else {
+        console.log('::PASSED::');
+      }
+    })
+    .catch((error) => {
+        console.error('Error running tests:', error);
+        console.log('::FAILED_TESTS::' + error.message);
+    });
 } catch (error) {
   console.error('Failed to read config or run tests:', error);
+  console.log('::FAILED_TESTS::' + error.message);
 }
